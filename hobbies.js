a51b370ae2fac8c13bf1b94f506a3171b66a08dd
marco-gulino <marcogulino98@gmail.com>
2025-01-31 15:55:36 +0100

    [feat/hobbies.js] created a function that generates an array of hobbies

---
+++ hobbies.js
@@ -0,0 +1,8 @@
+function hobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    const hobbiesList = [];
+    hobbiesList.push(hobbyOne, hobbyTwo, hobbyThree);
+    return hobbiesList;
+}
+
+const newHobbies = hobbies("Pescare", "Correre", "Modellismo");
+console.log(newHobbies);
